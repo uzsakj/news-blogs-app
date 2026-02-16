6f11f838509d2f91d4ef38398598ee8d416316df
uzsakj <feszoj18@gmail.com>
2026-02-16 22:36:02 +0100

    Add Font Awesome stylesheet, update App component structure, and create initial CSS styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import News from './components/News'
 
 const App = () => {
   return (
-    <div>
-      <News />
+    <div className='container'>
+      <div className="news-blog-app">
+        <News />
+      </div>
     </div>
   )
 }
