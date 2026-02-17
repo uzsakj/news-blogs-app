89b3fd642156ca3502d9206eaa4f4e0ea31a6134
uzsakj <feszoj18@gmail.com>
2026-02-17 20:46:49 +0100

    Add new form layout and styling for creating blog posts; update Blogs component to include image upload and text input fields.

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import './Blogs.css'
 import userImg from '../assets/images/user.jpg'
+import './Blogs.css'
 
 const Blogs = () => {
     return (
@@ -10,7 +10,21 @@ const Blogs = () => {
                 <p>Mary's Blog</p>
             </div>
             <div className="blogs-right">
-                <button className="post-btn">Create New Post</button>
+                {/* <button className="post-btn">Create New Post</button> */}
+                <div className="blogs-right-form">
+                    <h1>New Post</h1>
+                    <form>
+                        <div className="img-upload">
+                            <label htmlFor="file-upload" className='file-upload'>
+                                <i className="bx bx-upload"></i>Upload Image
+                            </label>
+                            <input type="file" id="file-upload" className='file-input' />
+                        </div>
+                        <input type="text" placeholder='Add Title (Max 60 Characters)' className='title-input' />
+                        <textarea placeholder='Add Text' className='text-input' ></textarea>
+                        <button type="submit" className='submit-btn'>Submit Button</button>
+                    </form>
+                </div>
                 <button className="blogs-close-btn">
                     Back <i className="bx bx-chevron-right"></i>
                 </button>
