0b82b9d13e82b504010d2d7bd94df44b3b8d6b4e
uzsakj <feszoj18@gmail.com>
2026-02-17 18:55:10 +0100

    Add calendar component with navigation and styling enhancements; integrate Boxicons for improved UI elements.

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,9 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Calendar.css'
 
 const Calendar = () => {
+    const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    const monthsOfYear = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'];
+    const currentDate = new Date();
+
+    const [currentMonth, setCurrentMonth] = useState(currentDate.getMonth());
+    const [currentYear, setCurrentYear] = useState(currentDate.getFullYear());
+    const [currentDay] = useState(currentDate.getDate());
+
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+
+
+    const prevMonth = () => {
+        setCurrentMonth((prevMonth) => prevMonth === 0 ? 11 : prevMonth - 1);
+        setCurrentYear((prevYear) => currentMonth === 0 ? prevYear - 1 : prevYear);
+    }
+    const nextMonth = () => {
+        setCurrentMonth((nextMonth) => nextMonth === 11 ? 0 : nextMonth + 1);
+        setCurrentYear((nextYear) => currentMonth === 11 ? nextYear + 1 : nextYear);
+    }
+
+
     return (
-        <div className='calendar'>Calendar</div>
+        <div className='calendar'>
+            <div className="navigate-date">
+                <h2 className="month">{monthsOfYear[currentMonth]},</h2>
+                <h2 className="year">{currentYear}</h2>
+                <div className="buttons">
+                    <i className="bx bx-chevron-left" onClick={prevMonth}></i>
+                    <i className="bx bx-chevron-right" onClick={nextMonth}></i>
+                </div>
+            </div>
+            <div className="weekdays">
+                {daysOfWeek.map((day, index) => (
+                    <span key={index}>{day}</span>
+                ))}
+            </div>
+            <div className="days">
+                {[...Array(firstDayOfMonth).keys()].map((_, index) => (
+                    <span key={`empty-${index}`} />
+                ))}
+                {[...Array(daysInMonth).keys()].map((day) => (
+                    <span
+                        key={`day-${day + 1}`}
+                        className={
+                            currentDay === day + 1 &&
+                                currentMonth === currentDate.getMonth() &&
+                                currentYear === currentDate.getFullYear()
+                                ? 'current-day'
+                                : ''}
+                    >
+                        {day + 1}
+                    </span>
+                ))}
+            </div>
+        </div>
     )
 }
 
